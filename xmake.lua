d7fc87bbcd3e20aab04b3444b5560b9d6e34dae4
wr_in_suanjiayun-nvidia-node-0001 <18203537680@163.com>
2026-08-11 17:46:32 +0000

    完成任务4.1 NVIDIA构建配置

--- xmake.lua
+++ xmake.lua
@@ -37,6 +37,9 @@ target("llaisys-device")
     set_kind("static")
     add_deps("llaisys-utils")
     add_deps("llaisys-device-cpu")
+    if has_config("nv-gpu") then
+        add_deps("llaisys-device-nvidia")
+    end
 
     set_languages("cxx17")
     set_warnings("all", "error")
@@ -83,6 +86,9 @@ target_end()
 target("llaisys-ops")
     set_kind("static")
     add_deps("llaisys-ops-cpu")
+    if has_config("nv-gpu") then
+        add_deps("llaisys-ops-nvidia")
+    end
 
     set_languages("cxx17")
     set_warnings("all", "error")
---
+++ xmake/nvidia.lua
@@ -0,0 +1,35 @@
+target("llaisys-device-nvidia")
+    set_kind("static")
+    set_languages("cxx17")
+    set_warnings("all", "error")
+
+    add_files("../src/device/nvidia/*.cu")
+    add_cugencodes("native", "compute_75")
+    add_links("cudart")
+
+    if not is_plat("windows") then
+        add_cuflags("-Xcompiler=-fPIC")
+    end
+
+    on_install(function (target) end)
+target_end()
+
+
+target("llaisys-ops-nvidia")
+    set_kind("static")
+    add_deps("llaisys-tensor")
+
+    set_languages("cxx17")
+    set_warnings("all", "error")
+
+    -- CUDA operator sources will be added in later stages.
+    -- add_files("../src/ops/*/nvidia/*.cu")
+    add_cugencodes("native", "compute_75")
+    add_links("cudart", "cublas")
+
+    if not is_plat("windows") then
+        add_cuflags("-Xcompiler=-fPIC")
+    end
+
+    on_install(function (target) end)
+target_end()
